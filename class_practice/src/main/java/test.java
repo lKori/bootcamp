998706aa5ea815c4b17d435877bc7b56ae3863b0
lKori <yuy0841@gmail.com>
2022-12-20 09:26:31 +0900

    221220

--- class_practice/src/main/java/test.java
+++ class_practice/src/main/java/test.java
@@ -1,44 +1,23 @@
 package main.java;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        String str = "dsafdasfffffsgaaaaabbbscccdd";
+        int num = 5;
 
-        String result = "";
-
-        int count = 0;
-        String s = "";
-        for(int i = 0; i < str.length(); i++) {
-            s = String.valueOf(str.charAt(i));
-            count = repeatChar(str, str.charAt(i), i);
-            if(count >= 3) {
-                result = result + String.valueOf(count) + s;
-                i += count;
-                count = 0;
-            } else {
-                result = result + String.valueOf(str.charAt(i));
-                count = 0;
-            }
-        }
-
-//        return result;
-        System.out.println(result);
+        System.out.println(fibonacci(num));
     }
 
-    public static int repeatChar(String str, char c, int idx) {
-        String s = "";
-        if(str.length() >= idx + 1) {
-            s = String.valueOf(str.charAt(idx + 1));
+    public static int fibonacci(int num) {
+        if(num == 0) {
+            return 0;
         }
-        int count = 0;
-        do {
-            count++;
-            idx++;
-            s = String.valueOf(str.charAt(idx + 1));
-        }while(String.valueOf(c).equals(s));
+        if(num == 1) {
+            return 1;
+        }
 
-        return count + 1;
+        return fibonacci(num - 1) + fibonacci(num - 2);
     }
 }
